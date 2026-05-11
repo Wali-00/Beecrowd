ab4b3077251b3f53a961b3000835dcfe83787ff8
Wali-00 <waliu4574@gmail.com>
2026-05-12 00:50:31 +0600

    1002:- Area of a Circle

---
+++ 1002.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double R;
+    cin>>R;
+    double area = (3.14159 * (R * R));
+    cout<<"A="<<fixed<<setprecision(4)<<area<<endl;
+}
