acf45906357be7afd754d10181bf2ec36b63d8db
Wali-00 <waliu4574@gmail.com>
2026-05-12 01:09:21 +0600

    1008:- Salary

---
+++ 1008.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    double c;
+    cin>>a>>b>>c;
+    double salary = b*c;
+    cout<<"NUMBER = "<<a<<endl;
+    cout<<"SALARY = U$ "<<fixed<<setprecision(2)<<salary<<endl;
+}
