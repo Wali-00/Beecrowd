d73ee4430ae9c7eddb580944d8f5d1e8ed244c84
Wali-00 <waliu4574@gmail.com>
2026-05-12 01:01:58 +0600

    1006:- Average 2

---
+++ 1006.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double A,B,C;
+    cin>>A>>B>>C;
+    double MEDIA = ((A*2)+(B*3)+(C*5))/10;
+    cout<<"MEDIA = "<<fixed<<setprecision(1)<<MEDIA<<endl;
+}
