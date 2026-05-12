064a74b43ce39d21cf20204cfd29adfd46d50b59
Wali-00 <waliu4574@gmail.com>
2026-05-13 00:30:19 +0600

    1011:- Sphere

---
+++ 1011.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi=3.14159;
+    double R;
+    cin>>R;
+    double volume=((4/3.0)*pi*(R*R*R));
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<volume<<endl;
+    
+}
